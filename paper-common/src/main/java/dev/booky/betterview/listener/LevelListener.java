4f5611392714887434f9b0154772fc065215c06f
booky10 <boooky10@gmail.com>
2025-06-04 02:42:30 +0200

    Eagerly load dimensions on startup

--- paper-common/src/main/java/dev/booky/betterview/listener/LevelListener.java
+++ paper-common/src/main/java/dev/booky/betterview/listener/LevelListener.java
@@ -2,6 +2,8 @@ package dev.booky.betterview.listener;
 // Created by booky10 in BetterView (22:32 03.06.2025)
 
 import dev.booky.betterview.common.BvdManager;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldUnloadEvent;
@@ -14,6 +16,12 @@ public class LevelListener implements Listener {
 
     public LevelListener(BvdManager manager) {
         this.manager = manager;
+
+        // eagerly load all available dimensions once on startup to allow
+        // population of config file - every other dimension gets lazy loaded
+        for (World dimension : Bukkit.getWorlds()) {
+            this.manager.getLevel(dimension.key());
+        }
     }
 
     @EventHandler
