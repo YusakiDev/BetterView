c89c95df15830225d2100a1b65cbe0e467753a49
booky10 <boooky10@gmail.com>
2025-06-05 02:33:48 +0200

    Setup paper 1.21.4 subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
 include("common")
 
 include("paper-common", "paper")
-sequenceOf("1.21.1", "1.21.3")
+sequenceOf("1.21.1", "1.21.3", "1.21.4")
     .map { it.replace(".", "") }
     .forEach { include("paper-$it") }
 
